c6a60a1046b41ade6ffe37f760063ea86b2ed80c
cesarbahena <cabahenac@gmail.com>
2023-10-11 14:47:39 -0600

    refactor(db): integrate Flyway for automatic schema migrations
    Replace manual schema creation with Flyway migration management.
    DatabaseManager now automatically runs migrations on initialization,
    providing idempotent, versioned database updates.
    
    Added getDatabaseUrl() method for jOOQ DSLContext configuration.

--- src/main/java/com/bara/app/database/DatabaseManager.java
+++ src/main/java/com/bara/app/database/DatabaseManager.java
@@ -1,38 +1,68 @@
 package com.bara.app.database;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+/**
+ * Database manager using Flyway for schema migrations.
+ * Flyway handles all schema creation and versioning automatically.
+ */
 public class DatabaseManager {
 
     private static final String DB_URL = "jdbc:sqlite:bara.db";
+    private static boolean initialized = false;
 
+    /**
+     * Get a connection to the SQLite database.
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
 
+    /**
+     * Initialize the database using Flyway migrations.
+     * This method is idempotent - safe to call multiple times.
+     */
     public static void initializeDatabase() {
-        try (Connection conn = getConnection();
-             Statement stmt = conn.createStatement()) {
+        if (initialized) {
+            return;
+        }
 
-            String schemaSql = """
-                CREATE TABLE IF NOT EXISTS menu_items (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL,
-                    price REAL NOT NULL,
-                    description TEXT
-                );
-                """;
-            stmt.execute(schemaSql);
-            System.out.println("Database initialized successfully.");
+        try {
+            System.out.println("Initializing database with Flyway...");
 
-        } catch (SQLException e) {
-            System.err.println("Error initializing database: " + e.getMessage());
+            // Configure Flyway
+            Flyway flyway = Flyway.configure()
+                    .dataSource(DB_URL, null, null)
+                    .locations("classpath:db/migration")
+                    .baselineOnMigrate(true) // Allow migrating an existing database
+                    .validateOnMigrate(true)
+                    .load();
+
+            // Run migrations
+            int migrationsApplied = flyway.migrate().migrationsExecuted;
+
+            if (migrationsApplied > 0) {
+                System.out.println("Database initialized successfully. Applied " + migrationsApplied + " migration(s).");
+            } else {
+                System.out.println("Database is up to date. No migrations applied.");
+            }
+
+            initialized = true;
+
+        } catch (FlywayException e) {
+            System.err.println("Error initializing database with Flyway: " + e.getMessage());
+            throw new RuntimeException("Failed to initialize database", e);
         }
     }
 
+    /**
+     * Close a database connection.
+     */
     public static void closeConnection(Connection conn) {
         try {
             if (conn != null && !conn.isClosed()) {
@@ -41,5 +71,12 @@ public class DatabaseManager {
         } catch (SQLException e) {
             System.err.println("Error closing connection: " + e.getMessage());
         }
+    }
+
+    /**
+     * Get the database URL (useful for jOOQ DSLContext configuration).
+     */
+    public static String getDatabaseUrl() {
+        return DB_URL;
     }
 }
