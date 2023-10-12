b42c4e052119af1af44c8ebb1d71b627127d2c7b
cesarbahena <cabahenac@gmail.com>
2023-10-12 09:18:42 -0600

    build(deps): add Flyway and jOOQ with code generation
    Add Flyway 9.22.3 for database migration management and jOOQ 3.18.9
    for type-safe SQL queries. Configure jOOQ Gradle plugin to generate
    POJOs, DAOs, and Records from Flyway-managed schema.
    
    Resolved SQLite-specific configuration issue by omitting inputSchema
    (SQLite doesn't support schema namespacing). Added jOOQ generated
    sources to main source set for compilation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     application
     id("org.openjfx.javafxplugin") version "0.0.13" // For JavaFX
+    id("nu.studer.jooq") version "8.2" // For jOOQ code generation
+    id("org.jetbrains.kotlin.jvm") version "1.9.23" // Retained for compatibility
 }
 
 group = "com.bara"
@@ -12,12 +14,61 @@ repositories {
 }
 
 dependencies {
-    // SQLite
+    // SQLite JDBC Driver
     implementation("org.xerial:sqlite-jdbc:3.46.0.0")
+
+    // Flyway for database migrations
+    implementation("org.flywaydb:flyway-core:9.22.3")
+
+    // jOOQ Core
+    implementation("org.jooq:jooq:3.18.9")
+
+    // jOOQ Code Generator (for build-time use)
+    jooqGenerator("org.jooq:jooq-codegen:3.18.9")
+    jooqGenerator("org.jooq:jooq-meta-extensions:3.18.9")
+    jooqGenerator("org.xerial:sqlite-jdbc:3.46.0.0") // SQLite driver for jOOQ generation
+    jooqGenerator("org.slf4j:slf4j-simple:1.7.36") // Logging for jOOQ generation
+
 
     // Use JUnit Jupiter for testing.
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+jooq {
+    version.set("3.18.9")
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                jdbc.apply {
+                    driver = "org.sqlite.JDBC"
+                    url = "jdbc:sqlite:bara.db"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.sqlite.SQLiteDatabase"
+                        // SQLite doesn't support schemas, so we don't set inputSchema
+                        excludes = "flyway_schema_history"
+                    }
+                    target.apply {
+                        packageName = "com.bara.app.db.jooq"
+                        directory = "build/generated-src/jooq/main"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isPojos = true
+                        isImmutablePojos = false
+                        isFluentSetters = true
+                        isDaos = true
+                    }
+                }
+            }
+        }
+    }
 }
 
 application {
@@ -29,7 +80,13 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+    sourceSets {
+        main {
+            java.srcDir("build/generated-src/jooq/main")
+        }
+    }
 }
+
 
 javafx {
     version = "21"
