8f3454a04b3460c21810a10fb2d0e6793efb71c5
cesarbahena <cabahenac@gmail.com>
2023-10-13 11:42:23 -0600

    feat(db): add party composition and anonymous order support
    - Make customer_id nullable to support walk-in/anonymous orders
    - Add cluster_id for linking to unidentified customer patterns
    - Add party_size and party_composition for behavioral tracking
    - Add context capture (table_number, day_of_week, time_of_day)
    - Add order_name fallback for kitchen display of anonymous orders

--- src/main/resources/db/migration/V5__create_orders.sql
+++ src/main/resources/db/migration/V5__create_orders.sql
@@ -1,17 +1,30 @@
 -- Migration: Create orders and order_items tables
--- Description: Order management with customer tracking and delivery details
+-- Description: Order management with customer tracking, party composition, and pattern recognition
 
 CREATE TABLE orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    customer_id INTEGER NOT NULL,
-    customer_address_id INTEGER, -- May be NULL for in-store pickup orders
+    customer_id INTEGER,                   -- NULLABLE! (anonymous orders supported)
+    cluster_id INTEGER,                    -- Link to unidentified_customer_clusters
+    customer_address_id INTEGER,
 
-    -- Order type
+    -- Order classification
     order_type TEXT NOT NULL DEFAULT 'delivery' CHECK (order_type IN ('delivery', 'pickup', 'dine_in')),
 
+    -- Anonymous order fallback
+    order_name TEXT,                       -- "Juan" if customer_id IS NULL (for kitchen display)
+
+    -- Party composition (for analytics)
+    party_size INTEGER,
+    party_composition TEXT,                -- JSON: [{'age':'40s'},{'age':'40s'},{'age':'10-15'},{'age':'10-15'}]
+
+    -- Context capture (for pattern recognition)
+    table_number TEXT,                     -- "14" or "Mesa 14"
+    day_of_week TEXT,                      -- Auto-filled: 'Monday', 'Tuesday', etc.
+    time_of_day TEXT,                      -- Auto-filled: '19:30'
+
     -- Address snapshot (preserve delivery address at order time)
-    raw_address_snapshot TEXT, -- Raw address as it was when order was placed
-    formatted_address_snapshot TEXT, -- Formatted address snapshot
+    raw_address_snapshot TEXT,
+    formatted_address_snapshot TEXT,
 
     -- Order status
     status TEXT NOT NULL DEFAULT 'pending'
@@ -32,6 +45,13 @@ CREATE TABLE orders (
     -- Notes
     customer_notes TEXT,
     internal_notes TEXT,
+    staff_observations TEXT,               -- "Regular corner table, asked for extra salsa"
+
+    -- Sync fields (for future cloud integration)
+    sync_version INTEGER DEFAULT 1,
+    last_synced_at TEXT,
+    synced_to_cloud INTEGER DEFAULT 0 CHECK (synced_to_cloud IN (0, 1)),
+    cloud_id TEXT,
 
     -- Timestamps
     ordered_at TEXT NOT NULL DEFAULT (datetime('now')),
@@ -49,12 +69,12 @@ CREATE TABLE order_items (
     menu_item_id INTEGER NOT NULL,
 
     -- Item details (snapshot at order time)
-    item_name TEXT NOT NULL, -- Preserve name even if menu item changes
+    item_name TEXT NOT NULL,               -- Preserve name even if menu item changes
     item_price REAL NOT NULL CHECK (item_price >= 0), -- Preserve price at order time
 
     -- Quantity and customization
     quantity INTEGER NOT NULL CHECK (quantity > 0),
-    notes TEXT, -- Special requests (e.g., "no onions")
+    notes TEXT,                            -- Special requests (e.g., "no onions")
 
     -- Line item total
     line_total REAL NOT NULL CHECK (line_total >= 0),
@@ -70,11 +90,19 @@ CREATE TABLE order_items (
 
 -- Indexes for orders table
 CREATE INDEX idx_orders_customer_id ON orders(customer_id);
+CREATE INDEX idx_orders_cluster_id ON orders(cluster_id);
 CREATE INDEX idx_orders_status ON orders(status);
 CREATE INDEX idx_orders_payment_status ON orders(payment_status);
 CREATE INDEX idx_orders_ordered_at ON orders(ordered_at);
 CREATE INDEX idx_orders_customer_ordered_at ON orders(customer_id, ordered_at DESC);
 CREATE INDEX idx_orders_type ON orders(order_type);
+
+-- Pattern matching indexes
+CREATE INDEX idx_orders_party_pattern ON orders(party_size, day_of_week, time_of_day);
+CREATE INDEX idx_orders_anonymous ON orders(order_name, party_size) WHERE customer_id IS NULL;
+
+-- Sync status
+CREATE INDEX idx_orders_sync_pending ON orders(synced_to_cloud) WHERE synced_to_cloud = 0;
 
 -- Indexes for order_items table
 CREATE INDEX idx_order_items_order_id ON order_items(order_id);
